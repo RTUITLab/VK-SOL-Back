aa0b26b94176fec1e985c958688616554b3b586b
cnrprod-team-27556 <sashalev200149@gmail.com>
2023-02-18 11:00:41 +0300

    Add user_id to event

--- models/Event.py
+++ models/Event.py
@@ -8,6 +8,7 @@ class Event(BaseModel):
     date: str
     place: str
     cover: str | None
+    user_id: str
     description: str
     amount: int     # Amount of tickets
     white_list: list[str] | None
